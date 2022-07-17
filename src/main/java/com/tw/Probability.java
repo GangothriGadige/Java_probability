29660e1dc0fbf9439ed52dc81d57dee3bc7d6a58
Gangothri <gangothrigadige985@gmail.com>
2022-07-17 21:51:47 +0530

    #Requirement 1 -Implemented a method to know probability of heads is equal to probability of tails in a coin toss

--- src/main/java/com/tw/Probability.java
+++ src/main/java/com/tw/Probability.java
@@ -1,4 +1,24 @@
 package com.tw;
 
+import java.util.Objects;
+
 public class Probability {
+    final double value;
+
+    public Probability(double probabilityOfAnEvent) {
+        this.value = probabilityOfAnEvent;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Probability that = (Probability) object;
+        return Double.compare(that.value, value) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
--- src/test/java/com/tw/ProbabilityTest.java
+++ src/test/java/com/tw/ProbabilityTest.java
@@ -1,4 +1,18 @@
 package com.tw;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class ProbabilityTest {
+    @Test
+    void shouldCheckEqualityWhenProbabilityOfHeadsIsComparedWithProbabilityOfTails() {
+        Probability probabilityOfHeads = new Probability(0.5);
+        Probability probabilityOfTails = new Probability(0.5);
+
+        assertThat(probabilityOfHeads, is(equalTo(probabilityOfTails)));
+    }
+
 }
